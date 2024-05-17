ae61cb2094ab5dc54dd097fed5126932eb976117
victorjudysen <117171668+victorjudysen@users.noreply.github.com>
2024-05-17 08:11:17 +0300

    major changes

--- srms/validate_login.php
+++ srms/validate_login.php
@@ -1,36 +1,24 @@
 <?php
 session_start();
-error_reporting(0);
 include('includes/config.php');
 
-if(isset($_POST['username']) && isset($_POST['password'])) {
-    $username = $_POST['username'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $childName = $_POST['username'];
     $password = $_POST['password'];
 
-    // Fetch user record from the database based on the username (student's surname)
-    $sql = "SELECT * FROM tblstudents WHERE StudentName = :username";
+    $sql = "SELECT * FROM parent_temp_password WHERE child_name = :childName AND temp_password = :password";
     $query = $dbh->prepare($sql);
-    $query->bindParam(':username', $username, PDO::PARAM_STR);
+    $query->bindParam(':childName', $childName, PDO::PARAM_STR);
+    $query->bindParam(':password', $password, PDO::PARAM_STR);
     $query->execute();
-    $result = $query->fetch(PDO::FETCH_ASSOC);
 
-    if($result !== false && password_verify($password, $result['Password'])) {
-        // Username and password are correct, set session variables and redirect to the results page
-        $_SESSION['student_id'] = $result['StudentId'];
-        $_SESSION['student_name'] = $result['StudentName'];
-        $_SESSION['class_id'] = $result['ClassId'];
-        $_SESSION['logged_in'] = true;
-        header('Location: result.php');
+    if ($query->rowCount() > 0) {
+        $_SESSION['child_name'] = $childName;
+        header("Location: result.php");
         exit;
     } else {
-        // Invalid username or password, redirect back to the login page with an error message
-        $_SESSION['error'] = 'Invalid username or password';
-        header('Location: find_results.php');
-        exit;
+        echo "<script>alert('Invalid Details');</script>";
+        echo "<script type='text/javascript'> document.location = 'find-result.php'; </script>";
     }
-} else {
-    // Redirect back to the login page if the form was not submitted properly
-    header('Location: find_results.php');
-    exit;
 }
 ?>
