61db282994107b747e78a253468fb3b6d9f3b31a
victorjudysen <117171668+victorjudysen@users.noreply.github.com>
2024-05-18 18:00:06 +0300

    Update admin-dashboard.php

--- srms/admin-dashboard.php
+++ srms/admin-dashboard.php
@@ -46,17 +46,17 @@ if(strlen($_SESSION['alogin'])=="")
                             <div class="container-fluid">
                                 <div class="row">
                                     <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                        <a class="dashboard-stat bg-primary" href="manage-students.php">
+                                        <a class="dashboard-stat bg-primary">
 <?php
-$sql1 ="SELECT StudentId from tblstudents ";
+$sql1 ="SELECT id from staff ";
 $query1 = $dbh -> prepare($sql1);
 $query1->execute();
 $results1=$query1->fetchAll(PDO::FETCH_OBJ);
-$totalstudents=$query1->rowCount();
+$totalstaff=$query1->rowCount();
 ?>
 
-                                            <span class="number counter"><?php echo htmlentities($totalstudents);?></span>
-                                            <span class="name">Registered Students</span>
+                                            <span class="number counter"><?php echo htmlentities($totalstaff);?></span>
+                                            <span class="name">Total Staff</span>
                                             <span class="bg-icon"><i class="fa fa-users"></i></span>
                                         </a>
                                         <!-- /.dashboard-stat -->
@@ -64,66 +64,28 @@ $totalstudents=$query1->rowCount();
                                     <!-- /.col-lg-3 col-md-3 col-sm-6 col-xs-12 -->
 
                                     <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                        <a class="dashboard-stat bg-danger" href="manage-subjects.php">
+                                        <a class="dashboard-stat bg-danger">
 <?php
 $sql ="SELECT id from  tblsubjects ";
 $query = $dbh -> prepare($sql);
 $query->execute();
 $results=$query->fetchAll(PDO::FETCH_OBJ);
-$totalsubjects=$query->rowCount();
+$totalparents=$query->rowCount();
 ?>
-                                            <span class="number counter"><?php echo htmlentities($totalsubjects);?></span>
-                                            <span class="name">Subjects Listed</span>
+                                            <span class="number counter"><?php echo htmlentities($totalparents);?></span>
+                                            <span class="name">Total Parents</span>
                                             <span class="bg-icon"><i class="fa fa-ticket"></i></span>
                                         </a>
                                         <!-- /.dashboard-stat -->
                                     </div>
                                     <!-- /.col-lg-3 col-md-3 col-sm-6 col-xs-12 -->
-
-                                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                        <a class="dashboard-stat bg-warning" href="manage-classes.php">
-                                        <?php
-$sql2 ="SELECT id from  tblclasses ";
-$query2 = $dbh -> prepare($sql2);
-$query2->execute();
-$results2=$query2->fetchAll(PDO::FETCH_OBJ);
-$totalclasses=$query2->rowCount();
-?>
-                                            <span class="number counter"><?php echo htmlentities($totalclasses);?></span>
-                                            <span class="name">Total classes listed</span>
-                                            <span class="bg-icon"><i class="fa fa-bank"></i></span>
-                                        </a>
-                                        <!-- /.dashboard-stat -->
-                                    </div>
-                                    <!-- /.col-lg-3 col-md-3 col-sm-6 col-xs-12 -->
-
-                                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                        <a class="dashboard-stat bg-success" href="manage-results.php">
-                                        <?php
-$sql3="SELECT  distinct StudentId from  tblresult ";
-$query3 = $dbh -> prepare($sql3);
-$query3->execute();
-$results3=$query3->fetchAll(PDO::FETCH_OBJ);
-$totalresults=$query3->rowCount();
-?>
-
-                                            <span class="number counter"><?php echo htmlentities($totalresults);?></span>
-                                            <span class="name">Results Uploaded</span>
-                                            <span class="bg-icon"><i class="fa fa-file-text"></i></span>
-                                        </a>
-                                        <!-- /.dashboard-stat -->
-                                    </div>
-                                    <!-- /.col-lg-3 col-md-3 col-sm-6 col-xs-12 -->
-
                                 </div>
                                 <!-- /.row -->
                             </div>
                             <!-- /.container-fluid -->
                         </section>
                         <!-- /.section -->
-
                 </div>
-
             </div>
         </div>
     </div>
